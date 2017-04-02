13be8a173b8e609282c2aa67aa16f69e080dab37
matt <matthew.mcgraw@gmail.com>
2017-04-01 22:14:12 -0400

    cleanup old code

--- Generator/codeswapper.py
+++ Generator/codeswapper.py
@@ -8,27 +8,14 @@ with open("../TestBlogContent/myblog.html", "r") as file_in:
   core_blog = file_in.read()
 
 sections_to_swap = re.findall(swap_pattern, core_blog)
-#print (sections_to_swap)
-
-
-#first_swap = core_blog.find(swap_key)
-#end_swap = core_blog[first_swap+len(swap_key):].find(end_key)
-
-#print (first_swap, end_swap)
-
-#section_to_swap = core_blog[first_swap:first_swap+len(swap_key)+end_swap+1]
-#print (section_to_swap)
 
 for section in sections_to_swap:
   print ("code to swap! {}".format(section))
-  #file_to_swap = (core_blog[first_swap+len(swap_key):first_swap+len(swap_key)+end_swap])
   file_match = re.search(file_pattern, section)
   file_to_swap = file_match.group()[1:-1]
   with open(folder_root+file_to_swap, "r") as new_content:
     new_html = new_content.read()
   new_blog = core_blog.replace(section, new_html)
 
-#print (new_blog)
-
-with open("new_blog.html", "w") as generated_blog:
+with open("{}new_blog.html".format(folder_root), "w") as generated_blog:
   generated_blog.write(new_blog)
