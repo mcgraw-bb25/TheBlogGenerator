fa1c3acb0790f0331fc30d510a8f7c562d1a3a64
matt <matthew.mcgraw@gmail.com>
2017-04-01 22:07:47 -0400

    adding pattern match on all matches

--- Generator/codeswapper.py
+++ Generator/codeswapper.py
@@ -1,7 +1,8 @@
 import re
 
 swap_pattern = "--codeswapper\[[A-Za-z0-9\._]+\]"
-#end_key = "]"
+file_pattern = "\[[A-Za-z0-9\._]+\]"
+folder_root = "../TestBlogContent/"
 
 with open("../TestBlogContent/myblog.html", "r") as file_in:
   core_blog = file_in.read()
@@ -18,14 +19,15 @@ sections_to_swap = re.findall(swap_pattern, core_blog)
 #section_to_swap = core_blog[first_swap:first_swap+len(swap_key)+end_swap+1]
 #print (section_to_swap)
 
-#if first_swap > -1:
-#  print ("code to swap!")
-#  file_to_swap = (core_blog[first_swap+len(swap_key):first_swap+len(swap_key)+end_swap])
-#  print (file_to_swap)
-#  with open(file_to_swap, "r") as new_content:
-#    new_html = new_content.read()
+for section in sections_to_swap:
+  print ("code to swap!")
+  #file_to_swap = (core_blog[first_swap+len(swap_key):first_swap+len(swap_key)+end_swap])
+  file_match = re.search(file_pattern, section)
+  file_to_swap = file_match.group()[1:-1]
+  with open(folder_root+file_to_swap, "r") as new_content:
+    new_html = new_content.read()
 
-##print (new_html)
+print (new_html)
 
 ##new_blog = core_blog.replace(section_to_swap, new_html)
 
