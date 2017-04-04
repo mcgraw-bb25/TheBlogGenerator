43f41e56cf378ae2ea48047a05fd329babe27d8e
matt <matthew.mcgraw@gmail.com>
2017-04-03 22:04:04 -0400

    moving in html generation

--- Generator/blog.py
+++ Generator/blog.py
@@ -1,18 +1,23 @@
+import markdown2
 
 
 class Blog:
   ''' This is the basic blog object.  Creating these will eventually become blog postings '''
 
-  def __init__(self, title, menu, markdown):
+  def __init__(self, title, menu, markdown, code_list=[]):
     ''' nothing for now '''
     self.title = title
     self.menu = menu # this will hold the menu to return to
     self.markdown = markdown # will hold reference to local markdown file
+    self.code_list = code_list # will hold list of code files to swap
     self.url = 'http://www.somesite.com/blog/{}.html'.format(self.title)
 
   def build_blog(self):
     ''' this will build and return html from markdown '''
-    pass
+    with open(self.markdown, "r") as file_to_read:
+      data = file_to_read.read()
+    html = markdown2.markdown(data, extras["footnotes"])
+    return html
 
   def swap_code(self):
     ''' this will swap out html with pygment code if necessary '''
