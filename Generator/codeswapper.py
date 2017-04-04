a68a32266ed49414d082f0cae05cf336b4f3f4cb
matt <matthew.mcgraw@gmail.com>
2017-04-03 22:51:50 -0400

    small fixes

--- Generator/codeswapper.py
+++ Generator/codeswapper.py
@@ -4,21 +4,15 @@ swap_pattern = "--codeswapper\[[A-Za-z0-9\._]+\]"
 file_pattern = "\[[A-Za-z0-9\._]+\]"
 folder_root = "../TestBlogContent/" # goes away too, but not yet
 
-# this section is removed if operating against a Blog
-with open("../TestBlogContent/myblog.html", "r") as file_in:
-  core_blog = file_in.read()
-
-
 def swap_code(html_in):
   ''' takes basic html blog and adds code section from pygment '''
-  sections_to_swap = re.findall(swap_pattern, html_on)
-
+  sections_to_swap = re.findall(swap_pattern, html_in)
+  html_out = html_in
   for section in sections_to_swap:
-    # print ("code to swap! {}".format(section))
     file_match = re.search(file_pattern, section)
     file_to_swap = file_match.group()[1:-1]
     with open(folder_root+file_to_swap, "r") as new_content:
       new_html = new_content.read()
-    html_out = core_blog.replace(section, new_html)
+    html_out = html_out.replace(section, new_html)
 
   return html_out
