c1eec51dc6216fe2d3787de5f2e8f5303ab89395
matt <matthew.mcgraw@gmail.com>
2017-04-07 22:12:57 -0400

    changes for testing and config

--- Generator/codeswapper.py
+++ Generator/codeswapper.py
@@ -4,14 +4,14 @@ swap_pattern = "--codeswapper\[[A-Za-z0-9\._]+\]"
 file_pattern = "\[[A-Za-z0-9\._]+\]"
 folder_root = "../TestBlogContent/" # goes away too, but not yet
 
-def swap_code(html_in):
+def swap_code(root_folder, html_in):
   ''' takes basic html blog and adds code section from pygment '''
   sections_to_swap = re.findall(swap_pattern, html_in)
   html_out = html_in
   for section in sections_to_swap:
     file_match = re.search(file_pattern, section)
     file_to_swap = file_match.group()[1:-1]
-    with open(folder_root+file_to_swap, "r") as new_content:
+    with open(root_folder+"/"+file_to_swap, "r") as new_content:
       new_html = new_content.read()
     html_out = html_out.replace(section, new_html)
 
