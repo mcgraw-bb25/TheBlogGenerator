d14c60182ed1170fd4864ac8f188fd421d5285b2
matt <matthew.mcgraw@gmail.com>
2017-04-09 13:17:00 -0400

    adding second test, two files

--- Tests/test_codeswapper.py
+++ Tests/test_codeswapper.py
@@ -11,5 +11,11 @@ class TestCodeSwapper(unittest.TestCase):
     swapped_html = swap_code(os.getcwd()+'/Tests', input_html)
     self.assertEqual(output_html, swapped_html)
 
+  def test_002(self):
+    output_html = "<html><h1>Test!</h1>print ('Hello, world!')\n print ('Hello, world!')\n <br>"
+    input_html = "<html><h1>Test!</h1>--codeswapper[test_codeswapper.html] --codeswapper[test_codeswapper.html] <br>"
+    swapped_html = swap_code(os.getcwd()+'/Tests', input_html)
+    self.assertEqual(output_html, swapped_html)
+
 if __name__ == "__main__":
   unittest.main()
