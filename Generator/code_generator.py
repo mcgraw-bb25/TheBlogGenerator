cb7f1d41a7dc38f5b38dcb23f1e17c70f9fb9dda
matt <matthew.mcgraw@gmail.com>
2017-04-16 15:53:22 -0400

    changing code_generator to accomodate tests without styles

--- Generator/code_generator.py
+++ Generator/code_generator.py
@@ -1,11 +1,14 @@
 import subprocess
 
 
-def generate_code_as_html(file_code_in, file_html_out):
+def generate_code_as_html(file_code_in, file_html_out, test_status=False):
   ''' takes in a file and generates code as html '''
   success = False
   try:
-    subprocess.run(["pygmentize", "-o", file_html_out, file_code_in], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if test_status:
+      subprocess.run(["pygmentize", "-o", file_html_out, file_code_in], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    else:
+      subprocess.run(["pygmentize", "-O", "full,style=colorful", "-O", "linenos=1", "-o", file_html_out, file_code_in], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     success = True
   except Exception as e:
     print ("generate_code_as_html() failed: {}".format(e))
--- Tests/test_code_generator.py
+++ Tests/test_code_generator.py
@@ -10,7 +10,7 @@ class TestCodeGenerator(unittest.TestCase):
   def test_001(self):
     file_code_in = os.getcwd() + "/Tests/pygment_test.py"
     file_html_out = os.getcwd() + "/Tests/pygment_test_output.html"
-    success = generate_code_as_html(file_code_in, file_html_out)
+    success = generate_code_as_html(file_code_in, file_html_out, test_status=True)
     with open(file_html_out, "r") as filein:
       output_html = filein.read()
 
